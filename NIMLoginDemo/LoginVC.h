dd92d125f08ed898aee97cca9f8d3adfe1aca3d7
KindWoo <wukaida@kindWoodeMacBook-Pro.local>
2019-05-04 20:17:52 +0800

    first commit

---
+++ NIMLoginDemo/LoginVC.h
@@ -0,0 +1,17 @@
+//
+//  LoginVC.h
+//  NIMLoginDemo
+//
+//  Created by KindWoo on 2019/5/4.
+//  Copyright © 2019 wukaida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
